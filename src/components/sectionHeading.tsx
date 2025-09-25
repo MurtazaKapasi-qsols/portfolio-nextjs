2f79ca22b7ba0fe447bad86e97389687afcbccf6
MurtazaKapasi-qsols <murtazakapasi.qsols@gmail.com>
2025-09-25 04:06:12 +0000

    spacing commit

--- src/components/sectionHeading.tsx
+++ src/components/sectionHeading.tsx
@@ -4,7 +4,7 @@ export default function SectionHeading({ title, subtitle, isSideHeading, classNa
     <p className={`text-gray-600 text-xs md:text-sm font-medium tracking-[4px] mb-4 ${className} uppercase`}>
         {subtitle}
     </p>
-    <h2 className={`text-lg md:text-5xl text-black mb-8 ${!isSideHeading && 'max-w-[50rem]'} mx-auto tracking-[10px] leading-[1.1] ${className} uppercase`}>
+    <h2 className={`text-lg md:text-5xl text-black mb-8 ${!isSideHeading && 'max-w-[50rem]'} mx-auto tracking-[3px] md:tracking-[7px] leading-[1.1] ${className} uppercase`}>
     {title}
     </h2>
         </>
